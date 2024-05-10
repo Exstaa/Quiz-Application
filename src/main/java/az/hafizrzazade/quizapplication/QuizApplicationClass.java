e8b03f818b4f5babf8238254b4976e829036c347
Exstaa <exstagithub@gmail.com>
2024-05-10 18:30:27 +0400

    Main class uploaded.

---
+++ src/main/java/az/hafizrzazade/quizapplication/QuizApplicationClass.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.quizapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApplicationClass {
+   public static void main(String[] args) {
+	   SpringApplication.run(QuizApplicationClass.class, args);
+   }
+}
